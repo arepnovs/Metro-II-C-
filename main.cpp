a3a6d4d41402c9bdeedf70855610f8378008482b
arepnovs <repnovant@gmail.com>
2017-05-19 13:33:40 +0300

    some optimisation of code

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ vector<string>      str_split_in_vect(string str, string delim, vector<string> s
     size_t pos;
     string stop;
 
-    while ((pos = str.find(delim)) != string::npos) {
+    while ((pos = str.find(delim)) != string::npos)
+    {
         stop = str.substr(0, pos);
         splits.push_back(stop);
         str.erase(0, pos + delim.length());
@@ -15,19 +16,51 @@ vector<string>      str_split_in_vect(string str, string delim, vector<string> s
 
 vector<t_stations>    stations_info(string line, int l, vector <t_stations> stations)
 {
-    int i = 0;
+    size_t i;
+    int j = 0;
+    size_t size;
     vector<string> all;
 
+    i = stations.size();
     all = str_split_in_vect(line, "-", all);
-    while (i < all.size())
+    size = all.size() + i;
+    while (i < size)
     {
         stations.push_back(s_stations());
-        stations[i].name = all[i];
+        stations[i].name = all[j];
         stations[i].line = l;
         if (i > 0)
-            stations[i].connections.push_back(all[i - 1]);
+            stations[i].connections.push_back(all[j - 1]);
         if (i < all.size() - 1)
-            stations[i].connections.push_back(all[i + 1]);
+            stations[i].connections.push_back(all[j + 1]);
+        i++;
+        j++;
+    }
+    return(stations);
+}
+
+vector<t_stations>    add_connections(vector<t_stations> stations, string str)
+{
+    int i = 0;
+    size_t pos;
+    string stop;
+    string delim = "-";
+    string conn[2];
+
+    while ((pos = str.find(delim)) != string::npos)
+    {
+        stop = str.substr(0, pos);
+        conn[i] = stop;
+        str.erase(0, pos + delim.length());
+        i++;
+    }
+    i = 0;
+    while (i < stations.size())
+    {
+        if (stations[i].name.compare(conn[0]))
+            stations[i].connections.push_back(conn[1]);
+        else if (stations[i].name.compare(conn[1]))
+            stations[i].connections.push_back(conn[0]);
         i++;
     }
     return(stations);
@@ -50,15 +83,14 @@ int main() {
                 getline(file, str);
                 stations = stations_info(str, line, stations);
                 all_stations = str_split_in_vect(str, "-", all_stations);
-                line++;
             }
             else if (str.find("#connect") != -1)
             {
                 while (getline(file, str))
                 {
-                    res = res + '|' + str;
+                    stations = add_connections(stations, str);
                 }
-                cout << "#connect " << res << endl;
+                //cout << "#connect " << res << endl;
             }
             else
                 cout << "Some input error" << endl;
