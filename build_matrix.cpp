e6bbca9ca519c887bdb542c6dcb8d864cac88ffa
arepnovs <repnovant@gmail.com>
2017-05-20 17:31:00 +0300

    works but need some cosmetic improvements

--- build_matrix.cpp
+++ build_matrix.cpp
@@ -6,6 +6,7 @@ void	get_stack_elements(t_dfs *path)
 
     i = 0;
     path->all[path->path_num] = (size_t *)malloc(sizeof(size_t) * path->size);
+    memset(path->all[path->path_num], 0, path->size);
     while (i < path->indx && path->path_num <= 100)
     {
         path->all[path->path_num][i] = path->path[i];
@@ -45,7 +46,8 @@ vector<t_stations>     get_start_and_dest(vector<t_stations> stations, int *stat
     if (flag == 1)
     {
         cout << "Enter start station" << endl;
-        do {
+        do
+        {
             getline(cin, input);
             while (i < stations.size())
             {
@@ -66,13 +68,18 @@ vector<t_stations>     get_start_and_dest(vector<t_stations> stations, int *stat
         if (station != 0)
             swap(stations[*station], stations[0]);
     }
-    else {
+    else
+    {
         cout << "Enter your destination" << endl;
-        do {
+        do
+        {
             getline(cin, input);
             while (i < stations.size()) {
                 if (stations[i].name.compare(input) == 0)
+                {
                     *station = i;
+                    break;
+                }
                 i++;
             }
             if (*station == -1)
@@ -80,9 +87,38 @@ vector<t_stations>     get_start_and_dest(vector<t_stations> stations, int *stat
                 cout << "Wrong input! Wrong station name" << endl;
                 i = 0;
             }
-        } while (*station == -1);
+        }
+        while (*station == -1);
     }
     return(stations);
+}
+
+void choose_best_path(size_t **all, int path_num, vector<t_stations> stations)
+{
+    int i = 0;
+    int len = 1000000;
+    int low_len = 1;
+    int best;
+
+    while (i < path_num)
+    {
+        while (all[i][low_len] != 0)
+            low_len++;
+        if (low_len < len)
+        {
+            len = low_len - 1;
+            best = i;
+        }
+        i++;
+        low_len =1;
+    }
+    i = 0;
+    while (i < len + 1)
+    {
+        cout << stations[all[best][i]].name << "  ";
+        i++;
+    }
+    cout << endl;
 }
 
 void	all_paths_search(vector<t_stations> stations, int **matrix)
@@ -97,20 +133,10 @@ void	all_paths_search(vector<t_stations> stations, int **matrix)
     path.indx = 0;
 
     stations = get_start_and_dest(stations, &dest, 0);
+    memset(path.visited, 0, path.size);
+    memset(path.path, 0, path.size);
     remember_path(0, dest, &path, matrix);
-    /*int i = 0;
-    int j;
-    while (i < path.path_num)
-    {
-        j = 0;
-        while (j < path.size)
-        {
-            cout << stations[path.all[i][j]].name << " ";
-            j++;
-        }
-        cout << endl;
-        i++;
-    }*/
+    choose_best_path(path.all, path.path_num, stations);
 }
 
 void find_way(vector<t_stations> stations)
@@ -134,18 +160,6 @@ void find_way(vector<t_stations> stations)
         }
         i++;
     }
-   /*i = 0;
-    while (i < stations.size())
-    {
-        j = 0;
-        while (j < stations.size())
-        {
-            cout << matrix[i][j];
-            j++;
-        }
-        cout << endl;
-        i++;
-    }*/
     all_paths_search(stations, matrix);
 }
 
